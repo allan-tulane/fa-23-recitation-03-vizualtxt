267838f3613ca5426e547550d1cff27a79d80b9e
vizualtxt <88392631+vizualtxt@users.noreply.github.com>
2023-09-20 19:10:52 -0500

    Update test_main.py
    finished lab3

--- test_main.py
+++ test_main.py
@@ -5,3 +5,6 @@ from main import *
 ## Feel free to add your own tests here.
 def test_multiply():
     assert quadratic_multiply(BinaryNumber(2), BinaryNumber(2)) == 2*2
+    assert quadratic_multiply(BinaryNumber(20), BinaryNumber(20)) == 20*20
+    assert quadratic_multiply(BinaryNumber(10), BinaryNumber(5)) == 10*5
+    assert quadratic_multiply(BinaryNumber(100), BinaryNumber(100)) == 100*100
